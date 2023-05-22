d995d2bd383e22bef506f3760c4c9b4eaef03120
Riofuad <imanmustika02@gmail.com>
2023-05-22 17:36:54 +0800

    Added Bottom NavBar and temporary icon, update com.google.android.material:material to 1.9.0, add viewBinding in gradle, add style for rounded image, add marketplace item card

--- mobile-development/app/src/main/java/academy/bangkit/sifresh/MainActivity.kt
+++ mobile-development/app/src/main/java/academy/bangkit/sifresh/MainActivity.kt
@@ -1,11 +1,18 @@
 package academy.bangkit.sifresh
 
+import academy.bangkit.sifresh.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
