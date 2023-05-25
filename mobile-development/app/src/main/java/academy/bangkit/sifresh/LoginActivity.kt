a02910cfa14ff07a22ca992575da28d3d6769c9f
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-05-25 20:20:35 +0700

    login activity ui

---
+++ mobile-development/app/src/main/java/academy/bangkit/sifresh/LoginActivity.kt
@@ -0,0 +1,17 @@
+package academy.bangkit.sifresh
+
+import academy.bangkit.sifresh.databinding.ActivityLoginBinding
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
