392c82846832f4c4d0b4fcd7b4ce009912f7c384
Irvan Malik <irvanmalik48@gmail.com>
2023-05-30 07:30:17 +0700

    feat(api/product): implement base product api
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ cloud-computing/src/api/product/delete.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import conn from "../../config/db";
+
+const router = express.Router();
+
+// Product schema:
+// id: varchar(255) (UUIDv4 using uuidjs) (primary key)
+// sellerId: varchar(255) (UUIDv4 using uuidjs) (foreign key to User.id)
+// name: varchar(255)
+// sellerName: varchar(100)
+// type: varchar(10) (enum: ["vegetable", "fruit", "other"])
+// price: int
+// isAvailable: boolean
+// description: text
+// productPicUrls: varchar(255) (array of strings)
+// publishedAt: date
+
+function deleteProductById(id: string) {
+  const sql = `
+    DELETE FROM Product WHERE id = ?;
+  `;
+
+  conn.execute(sql, [id], (err: any, result: any) => {
+    if (err) throw err;
+    return result;
+  });
+}
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const result = deleteProductById(id);
+
+  res.send(result);
+});
+
+export default router;
