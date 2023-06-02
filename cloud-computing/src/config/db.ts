4256b21209eb6c6e4f7e1ff1c17cdd86c7b420ba
Irvan Malik <irvanmalik48@gmail.com>
2023-06-02 15:03:59 +0700

    fix(db): the db fix, yeah, wrong env table name
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>
    Authored-by: muhammad irfan <m.irfan0205@gmail.com>

--- cloud-computing/src/config/db.ts
+++ cloud-computing/src/config/db.ts
@@ -1,17 +1,21 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
-const conn = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-});
+async function connection() {
+  const conn = await mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  });
 
-export default conn;
+  return conn;
+}
+
+export default connection;
