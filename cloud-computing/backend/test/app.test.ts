dea47ab224718736d98f1bcddd9378d9d120e535
Irvan Malik <irvanmalik48@gmail.com>
2023-06-05 22:43:49 +0700

    feat(backend): move backend to separate folders
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- cloud-computing/backend/test/app.test.ts
+++ cloud-computing/backend/test/app.test.ts
@@ -60,4 +60,44 @@ describe("User endpoints", () => {
         });
     });
   });
+
+  describe("GET /api/v1/user/get/email/:email", () => {
+    it("Responds with a JSON message and 200 status code", () => {
+      request(app)
+        .get("/api/v1/user/get/email/johndoe@example.com")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+
+    it("Responds with a JSON message and 404 status code if user not found", () => {
+      request(app)
+        .get("/api/v1/user/get/email/1")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(404, {
+          message: "User not found",
+        });
+    });
+  });
+
+  describe("GET /api/v1/user/get/phone/:phone", () => {
+    it("Responds with a JSON message and 200 status code", () => {
+      request(app)
+        .get("/api/v1/user/get/phone/1234567890")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+
+    it("Responds with a JSON message and 404 status code if user not found", () => {
+      request(app)
+        .get("/api/v1/user/get/phone/1")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(404, {
+          message: "User not found",
+        });
+    });
+  });
 });
