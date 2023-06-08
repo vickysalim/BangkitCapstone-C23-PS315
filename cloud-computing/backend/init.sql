fcdb6f89dee21cc81a9052c53992694882019636
Irvan Malik <irvanmalik48@gmail.com>
2023-06-08 15:27:02 +0700

    feat(sql): update the schema
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>
    Co-authored-by: muhammad irfan <m.irfan0205@gmail.com>
    Co-authored-by: Irvan Malik <irvanmalik48@gmail.com>

--- cloud-computing/backend/init.sql
+++ cloud-computing/backend/init.sql
@@ -1,96 +1,259 @@
-CREATE TABLE `User` (
-  `id` varchar(255) PRIMARY KEY,
-  `fullName` varchar(100),
-  `email` varchar(75),
-  `password` varchar(255),
-  `phone` varchar(15),
-  `isSeller` boolean DEFAULT false,
-  `profilePicUrl` varchar(255) DEFAULT "/assets/user/default.jpeg"
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 08, 2023 at 08:26 AM
+-- Server version: 10.11.3-MariaDB
+-- PHP Version: 8.2.6
 
-CREATE TABLE `UserAddress` (
-  `id` varchar(255) PRIMARY KEY,
-  `address` text,
-  `province` varchar(75),
-  `city` varchar(75),
-  `kecamatan` varchar(75),
-  `kodePos` varchar(10)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `Product` (
-  `id` varchar(255) PRIMARY KEY,
-  `sellerId` varchar(255),
-  `name` varchar(255),
-  `sellerName` varchar(100),
-  `type` varchar(10),
-  `price` int,
-  `isAvailable` boolean,
-  `description` text,
-  `productPicUrls` varchar(255) COMMENT '[IS AN ARRAY]: Contains urls to images uploaded inside the public bucket.',
-  `publishedAt` date
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sifresh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CartItem`
+--
 
 CREATE TABLE `CartItem` (
-  `id` varchar(255) PRIMARY KEY,
-  `userId` varchar(255),
-  `productId` varchar(255),
-  `status` varchar(10) DEFAULT "draft" COMMENT 'It can be draft, send, or complete. If complete, delete item gracefully and asynchronously using triggers.',
-  `amount` int
-);
+  `id` varchar(255) NOT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `productId` varchar(255) DEFAULT NULL,
+  `status` varchar(10) DEFAULT 'draft' COMMENT 'It can be draft, send, or complete. If complete, delete item gracefully and asynchronously using triggers.',
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores individual cart item data.';
 
-CREATE TABLE `OrderHistory` (
-  `id` varchar(255) PRIMARY KEY,
-  `userId` varchar(255),
-  `orderDate` date,
-  `status` varchar(10) COMMENT 'Can be either processed, unpaid, delivering, or delivered.'
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `OrderItem` (
-  `id` varchar(255) PRIMARY KEY,
-  `orderId` varchar(255),
-  `cartItemId` varchar(255)
-);
-
-CREATE TABLE `Review` (
-  `id` varchar(255) PRIMARY KEY,
-  `productId` varchar(255),
-  `userId` varchar(255),
-  `rating` int,
-  `title` varchar(255),
-  `description` text
-);
+--
+-- Table structure for table `FreshnessDataset`
+--
 
 CREATE TABLE `FreshnessDataset` (
-  `id` varchar(255) PRIMARY KEY,
-  `userId` varchar(255),
-  `name` varchar(255),
-  `type` varchar(10),
-  `isFresh` boolean,
-  `nutritionDesc` text
-);
+  `id` varchar(255) NOT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `productId` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `isFresh` tinyint(1) DEFAULT NULL,
+  `nutritionDesc` text DEFAULT NULL,
+  `pictureUrl` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `User` COMMENT = 'Stores user data.';
+-- --------------------------------------------------------
 
-ALTER TABLE `Product` COMMENT = 'Stores product data.';
+--
+-- Table structure for table `OrderHistory`
+--
 
-ALTER TABLE `CartItem` COMMENT = 'Stores individual cart item data.';
+CREATE TABLE `OrderHistory` (
+  `id` varchar(255) NOT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `orderDate` date DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL COMMENT 'Can be either processed, unpaid, delivering, or delivered.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `UserAddress` ADD FOREIGN KEY (`id`) REFERENCES `User` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Product` ADD FOREIGN KEY (`sellerId`) REFERENCES `User` (`id`);
+--
+-- Table structure for table `OrderItem`
+--
 
-ALTER TABLE `CartItem` ADD FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+CREATE TABLE `OrderItem` (
+  `id` varchar(255) NOT NULL,
+  `orderId` varchar(255) DEFAULT NULL,
+  `cartItemId` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `CartItem` ADD FOREIGN KEY (`productId`) REFERENCES `Product` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `OrderHistory` ADD FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+--
+-- Table structure for table `Product`
+--
 
-ALTER TABLE `OrderItem` ADD FOREIGN KEY (`orderId`) REFERENCES `OrderHistory` (`id`);
+CREATE TABLE `Product` (
+  `id` varchar(255) NOT NULL,
+  `sellerId` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `sellerName` varchar(100) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `isAvailable` tinyint(1) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `productPicUrls` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '[IS AN ARRAY]: Contains urls to images uploaded inside the public bucket.' CHECK (json_valid(`productPicUrls`)),
+  `publishedAt` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores product data.';
 
-ALTER TABLE `OrderItem` ADD FOREIGN KEY (`cartItemId`) REFERENCES `CartItem` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Review` ADD FOREIGN KEY (`productId`) REFERENCES `Product` (`id`);
+--
+-- Table structure for table `Review`
+--
 
-ALTER TABLE `Review` ADD FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+CREATE TABLE `Review` (
+  `id` varchar(255) NOT NULL,
+  `productId` varchar(255) DEFAULT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `FreshnessDataset` ADD FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` varchar(255) NOT NULL,
+  `fullName` varchar(100) DEFAULT NULL,
+  `email` varchar(75) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `isSeller` tinyint(1) DEFAULT 0,
+  `profilePicUrl` varchar(255) DEFAULT '/assets/user/default.jpeg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores user data.';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `UserAddress`
+--
+
+CREATE TABLE `UserAddress` (
+  `id` varchar(255) NOT NULL,
+  `address` text DEFAULT NULL,
+  `province` varchar(75) DEFAULT NULL,
+  `city` varchar(75) DEFAULT NULL,
+  `kecamatan` varchar(75) DEFAULT NULL,
+  `kodePos` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `CartItem`
+--
+ALTER TABLE `CartItem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `productId` (`productId`);
+
+--
+-- Indexes for table `FreshnessDataset`
+--
+ALTER TABLE `FreshnessDataset`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `productId` (`productId`);
+
+--
+-- Indexes for table `OrderHistory`
+--
+ALTER TABLE `OrderHistory`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `OrderItem`
+--
+ALTER TABLE `OrderItem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderId` (`orderId`),
+  ADD KEY `cartItemId` (`cartItemId`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sellerId` (`sellerId`);
+
+--
+-- Indexes for table `Review`
+--
+ALTER TABLE `Review`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productId` (`productId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `UserAddress`
+--
+ALTER TABLE `UserAddress`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `CartItem`
+--
+ALTER TABLE `CartItem`
+  ADD CONSTRAINT `CartItem_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`id`),
+  ADD CONSTRAINT `CartItem_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `Product` (`id`);
+
+--
+-- Constraints for table `FreshnessDataset`
+--
+ALTER TABLE `FreshnessDataset`
+  ADD CONSTRAINT `FreshnessDataset_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `OrderHistory`
+--
+ALTER TABLE `OrderHistory`
+  ADD CONSTRAINT `OrderHistory_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `OrderItem`
+--
+ALTER TABLE `OrderItem`
+  ADD CONSTRAINT `OrderItem_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `OrderHistory` (`id`),
+  ADD CONSTRAINT `OrderItem_ibfk_2` FOREIGN KEY (`cartItemId`) REFERENCES `CartItem` (`id`);
+
+--
+-- Constraints for table `Product`
+--
+ALTER TABLE `Product`
+  ADD CONSTRAINT `Product_ibfk_1` FOREIGN KEY (`sellerId`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Review`
+--
+ALTER TABLE `Review`
+  ADD CONSTRAINT `Review_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `Product` (`id`),
+  ADD CONSTRAINT `Review_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `UserAddress`
+--
+ALTER TABLE `UserAddress`
+  ADD CONSTRAINT `UserAddress_ibfk_1` FOREIGN KEY (`id`) REFERENCES `User` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
