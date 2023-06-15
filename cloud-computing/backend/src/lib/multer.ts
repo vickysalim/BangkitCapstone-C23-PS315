6f42509ec87b5203c76fb93914de1e40f349c7fa
Irvan Malik <irvanmalik48@gmail.com>
2023-06-15 19:14:57 +0700

    feat(multer): update naming convention, and actually fix some derp
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- cloud-computing/backend/src/lib/multer.ts
+++ cloud-computing/backend/src/lib/multer.ts
@@ -1,5 +1,6 @@
 import multer from "multer";
 import MulterGoogleCloudStorage from "multer-cloud-storage";
+import { uuidv4 } from "./uuid";
 
 const upload = multer({
   storage: new MulterGoogleCloudStorage({
@@ -7,7 +8,7 @@ const upload = multer({
     destination: "uploads/",
     projectId: process.env.GOOGLE_CLOUD_PROJECT,
     filename: (req: { body: { id: any; }; }, file: { originalname: any; }, cb: (arg0: null, arg1: string) => void) => {
-      const fileName = `${Date.now()}-${req.body.id}-${file.originalname}`;
+      const fileName = `${Date.now()}-${req.body.id ?? uuidv4()}-${file.originalname}`;
       cb(null, fileName);
     },
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
@@ -15,7 +16,7 @@ const upload = multer({
   }),
   dest: "uploads/",
   limits: {
-    fileSize: 12 * 1024 * 1024, // no larger than 5mb
+    fileSize: 12 * 1024 * 1024, // no larger than 12mb
   },
 });
 
