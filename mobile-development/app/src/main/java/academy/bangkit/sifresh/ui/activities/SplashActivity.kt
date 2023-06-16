7777b0cf7390fe8d6134892a16646e6fd650735e
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-06-16 12:10:53 +0700

    splash screen

--- mobile-development/app/src/main/java/academy/bangkit/sifresh/ui/activities/SplashActivity.kt
+++ mobile-development/app/src/main/java/academy/bangkit/sifresh/ui/activities/SplashActivity.kt
@@ -1,11 +1,16 @@
 package academy.bangkit.sifresh.ui.activities
 
+import academy.bangkit.sifresh.data.local.SettingPreferences
+import academy.bangkit.sifresh.data.local.dataStore
 import academy.bangkit.sifresh.databinding.ActivitySplashBinding
+import academy.bangkit.sifresh.ui.viewmodels.SettingViewModel
+import academy.bangkit.sifresh.ui.viewmodels.SettingViewModelFactory
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.ViewModelProvider
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
@@ -18,10 +23,23 @@ class SplashActivity : AppCompatActivity() {
         Handler(
             Looper.getMainLooper()
         ).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            getUserPreferences()
             finish()
         }, SPLASH_DURATION_MILLIS)
+    }
+
+    private fun getUserPreferences() {
+        val settingPreferences = SettingPreferences.getInstance(this.dataStore)
+        val settingViewModel = ViewModelProvider(
+            this,
+            SettingViewModelFactory(settingPreferences)
+        )[SettingViewModel::class.java]
+
+        settingViewModel.getUserPreferences(SettingPreferences.Companion.UserPreferences.UserToken.name)
+            .observe(this) { token ->
+                if (token != "") startActivity(Intent(this, MainActivity::class.java))
+                else startActivity(Intent(this, LoginActivity::class.java))
+            }
     }
 
     companion object {
